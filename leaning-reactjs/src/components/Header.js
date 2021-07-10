3ea94278f6fd5e33009afeb332694d581f505f99
mvn-lieuphan-dn <lieu.phan@monstar-lab.com>
2021-07-11 00:28:04 +0700

    Complete exercise day 6

--- leaning-reactjs/src/components/Header.js
+++ leaning-reactjs/src/components/Header.js
@@ -6,17 +6,10 @@ export default function Header() {
         <div className="header-left flex">
           <img src={logo} className="header-logo" alt="logo"/>
           <ul className="menu-list flex">
-            <li className="menu-item"><a className="menu-item-link" href="#">Docs</a></li>
-            <li className="menu-item"><a className="menu-item-link" href="#">Tutorial</a></li>
-            <li className="menu-item"><a className="menu-item-link" href="#">Blog</a></li>
-            <li className="menu-item"><a className="menu-item-link" href="#">Community</a></li>
+            <li className="menu-item flex justify-content-center align-items-center"><a className="menu-item-link" href="#">List</a></li>
+            <li className="menu-item flex justify-content-center align-items-center"><a className="menu-item-link active" href="#">Group</a></li>
           </ul>
         </div>
-        <ul className="header-right flex">
-          <li><a href="#"><i className="fa fa-facebook-f"></i></a></li>
-          <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-          <li><a href="#"><i className="fa fa-google"></i></a></li>
-        </ul>
       </div>
     </header>
   )
